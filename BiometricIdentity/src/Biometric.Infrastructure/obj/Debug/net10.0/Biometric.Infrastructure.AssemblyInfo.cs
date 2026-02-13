be522c9b6e1e2b772d8b330193c4192c78a17f4d
mololuwa10 <mololuwasegi@yahoo.com>
2026-02-13 17:32:34 -0600

    `Updated AssemblyInformationalVersion attribute in Biometric.Infrastructure project.`

--- BiometricIdentity/src/Biometric.Infrastructure/obj/Debug/net10.0/Biometric.Infrastructure.AssemblyInfo.cs
+++ BiometricIdentity/src/Biometric.Infrastructure/obj/Debug/net10.0/Biometric.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biometric.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac5f2f0514a4b87ead345c44be45a8ffdb66be6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8b245db6808dfca06a653561fb65f221158a5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biometric.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biometric.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
